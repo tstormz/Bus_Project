9b8fa9abf59e960fcc20b97311fe04bdfb94ce5d
Travis Hafner <travisdhafner@gmail.com>
2015-04-16 17:32:23 -0400

    Update CentroTRMap.java

--- CentroTRMap.java
+++ CentroTRMap.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.graphics.Color;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,7 +15,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class CentroTRMap extends ActionBarActivity {
 
@@ -21,8 +38,35 @@ public class CentroTRMap extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_centro_trmap);
-        setUpMapIfNeeded();
+        mMap.setMyLocationEnabled(true);
+        MarkerOptions options = new MarkerOptions();
+        options.position(new LatLng(43.453838, -76.540628));
+        options.position(new LatLng(43.460593, -76.492418));
+        mMap.addMarker(options);
+        String url = getMapsApiDirectionsUrl();
+        ReadTask downloadTask = new ReadTask();
+		downloadTask.execute(url);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.453838, -76.540628), (float) 14.5));
+        // addMarkers();
     }
+    
+    private void addMarkers() {
+		if (mMap != null) {
+			mMap.addMarker(new MarkerOptions().position(BROOKLYN_BRIDGE)
+					.title("First Point"));
+			mMap.addMarker(new MarkerOptions().position(LOWER_MANHATTAN)
+					.title("Second Point"));
+		}
+    
+    private String getMapsApiDirectionsUrl() {
+		String waypoints = "waypoints=optimize:true|43.453838,-76.540628|43.460593,-76.492418";
+		String sensor = "sensor=false";
+		String params = waypoints + "&" + sensor;
+		String output = "json";
+		String url = "https://maps.googleapis.com/maps/api/directions/"
+				+ output + "?" + params;
+		return url;
+	}
 
    // @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,12 +96,143 @@ public class CentroTRMap extends ActionBarActivity {
     }
 
     private void setUpMap() {
-        mMap.setMyLocationEnabled(true);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.453838, -76.540628), (float) 14.5));
-        mMap.addMarker(new MarkerOptions()
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.appicon))
-                .title("Oswego")
-                .snippet("The most awesomest city in NY.")
-                .position(new LatLng(43.453838, -76.540628)));
+        
     }
+    
+    	private class ReadTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... url) {
+			String data = "";
+			try {
+				HttpConnection http = new HttpConnection();
+				data = http.readUrl(url[0]);
+			} catch (Exception e) {
+				Log.d("Background Task", e.toString());
+			}
+			return data;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			new ParserTask().execute(result);
+		}
+	}
+
+
+public class HttpConnection {
+	public String readUrl(String mapsApiDirectionsUrl) throws IOException {
+		String data = "";
+		InputStream iStream = null;
+		HttpURLConnection urlConnection = null;
+		try {
+			URL url = new URL(mapsApiDirectionsUrl);
+			urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.connect();
+			iStream = urlConnection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					iStream));
+			StringBuffer sb = new StringBuffer();
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+			data = sb.toString();
+			br.close();
+		} catch (Exception e) {
+			Log.d("Exception while reading url", e.toString());
+		} finally {
+			iStream.close();
+			urlConnection.disconnect();
+		}
+		return data;
+	}
+
+}
+
+public class PathJSONParser {
+
+	public List<List<HashMap<String, String>>> parse(JSONObject jObject) {
+		List<List<HashMap<String, String>>> routes = new ArrayList<List<HashMap<String, String>>>();
+		JSONArray jRoutes = null;
+		JSONArray jLegs = null;
+		JSONArray jSteps = null;
+		try {
+			jRoutes = jObject.getJSONArray("routes");
+			/** Traversing all routes */
+			for (int i = 0; i < jRoutes.length(); i++) {
+				jLegs = ((JSONObject) jRoutes.get(i)).getJSONArray("legs");
+				List<HashMap<String, String>> path = new ArrayList<HashMap<String, String>>();
+
+				/** Traversing all legs */
+				for (int j = 0; j < jLegs.length(); j++) {
+					jSteps = ((JSONObject) jLegs.get(j)).getJSONArray("steps");
+
+					/** Traversing all steps */
+					for (int k = 0; k < jSteps.length(); k++) {
+						String polyline = "";
+						polyline = (String) ((JSONObject) ((JSONObject) jSteps
+								.get(k)).get("polyline")).get("points");
+						List<LatLng> list = decodePoly(polyline);
+
+						/** Traversing all points */
+						for (int l = 0; l < list.size(); l++) {
+							HashMap<String, String> hm = new HashMap<String, String>();
+							hm.put("lat",
+									Double.toString(((LatLng) list.get(l)).latitude));
+							hm.put("lng",
+									Double.toString(((LatLng) list.get(l)).longitude));
+							path.add(hm);
+						}
+					}
+					routes.add(path);
+				}
+			}
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+		}
+		return routes;
+	}
+
+	/**
+	 * Method Courtesy :
+	 * jeffreysambells.com/2010/05/27
+	 * /decoding-polylines-from-google-maps-direction-api-with-java
+	 * */
+	private List<LatLng> decodePoly(String encoded) {
+
+		List<LatLng> poly = new ArrayList<LatLng>();
+		int index = 0, len = encoded.length();
+		int lat = 0, lng = 0;
+
+		while (index < len) {
+			int b, shift = 0, result = 0;
+			do {
+				b = encoded.charAt(index++) - 63;
+				result |= (b & 0x1f) << shift;
+				shift += 5;
+			} while (b >= 0x20);
+			int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+			lat += dlat;
+
+			shift = 0;
+			result = 0;
+			do {
+				b = encoded.charAt(index++) - 63;
+				result |= (b & 0x1f) << shift;
+				shift += 5;
+			} while (b >= 0x20);
+			int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+			lng += dlng;
+
+			LatLng p = new LatLng((((double) lat / 1E5)),
+					(((double) lng / 1E5)));
+			poly.add(p);
+		}
+		return poly;
+	}
+}
+
 }
